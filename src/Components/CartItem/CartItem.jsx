a3084a6912ec2cd1456c623f1ed2a0e1d7b5cb80
faisal <mdfaisal51777@gmail.com>
2024-11-17 11:34:31 +0600

    added an Footer

--- src/Components/CartItem/CartItem.jsx
+++ src/Components/CartItem/CartItem.jsx
@@ -150,12 +150,27 @@ const CartItem = () => {
                   <p className="font-medium">Price: ${item.price}</p>
                 </div>
               </div>
-              <button
-                onClick={() => handleRemoveFromCart(item.product_id)}
-                className="btn btn-circle btn-xs border-red-500 text-red-500 bg-white mt-10 mr-20"
-              >
-                Remove
-              </button>
+              <div>
+                <button
+                  onClick={() => handleRemoveFromCart(item.product_id)}
+                  className="btn btn-circle btn-xs border-red-500 text-red-500 bg-white mt-5 mr-20"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </div>
             </div>
           ))
         ) : (
